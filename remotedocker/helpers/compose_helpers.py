cb7c09c40dbc27629a01ec76214eb893a2f5c50b
Jakub Schier <jakub.schier@gmail.com>
2021-04-18 21:31:33 +0200

    Implement loading the compose and persisting it

--- remotedocker/helpers/compose_helpers.py
+++ remotedocker/helpers/compose_helpers.py
@@ -4,9 +4,13 @@ import logging
 L = logging.getLogger(__name__)
 
 class DockerCompose:
-	def __init__(compose_path):
-		with open(compose_path) as file:
-			self.docker_compose = yaml.load(file, Loader=yaml.FullLoader)
+	def __init__(compose_path=None):
+		self.compose_path = compose_path
+		if self.compose_path:
+			with open(self.compose_path) as file:
+				self.docker_compose = yaml.load(file, Loader=yaml.FullLoader)
+		else:
+			self.docker_compose = {}
 
 	def docker_compose_list_current(self):
 		return self.docker_compose
@@ -19,3 +23,7 @@ class DockerCompose:
 
 	def docker_compose_remove(self, to_remove: str):
 		pass
+
+	def docker_compose_persist(self):
+		with open(self.compose_path, "w") as file:
+			file.write(yaml.dump(self.docker_compose, sort_keys=False))
